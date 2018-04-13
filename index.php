885874b758971506fa52bea982fa03ec438d0911
Brent Kie <brent.a.kie@gmail.com>
2018-04-13 12:59:15 -0600

    User persona created for Data Design assignment

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+	</head>
+	<body>
+
+	<h1>Data Design Project</h1>
+
+		<h2>User Persona</h2>
+			<ul>
+				<li><strong>Name:</strong> Jane Locke</li>
+				<li><strong>Age:</strong> 22</li>
+				<li><strong>Gender:</strong> Female</li>
+				<li><strong>Occupation:</strong> Bank Teller</li>
+				<li><strong>Technology:</strong> Phone; Samsung Galaxy S8. Laptop: Microsoft Surface Book</li>
+				<li><strong>Attitude:</strong> Likes to leave comments about latest releases, prefers PayPal payment, enjoys hearing similar artists/genres</li>
+				<li><strong>Needs/Goals/Features:</strong></li>
+					<ul>
+						<li>Ability to leave reviews/comments on new releases</li>
+						<li>See others who purchased the album in order to connect with other fans.</li>
+						<li>Wants to preview music before making purchase</li>
+					</ul>
+				<li><strong>Frustrations:</strong></li>
+					<ul>
+						<li>Inablity to purchase music via mobile app</li>
+						<li></li>
+					</ul>
+			</ul>
+	</body>
+</html>
