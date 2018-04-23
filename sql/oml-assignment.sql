4dad8b577a369b964996af5919c7e7c4d1dfacf8
Brent Kie <brent.a.kie@gmail.com>
2018-04-23 17:55:55 -0600

    added INSERT, UPDATE, and DELETE statments

---
+++ sql/oml-assignment.sql
@@ -0,0 +1,12 @@
+INSERT INTO fan (fanId, fanActivationToken, fanEmail, fanHash, fanUsername)
+VALUES (UNHEX(REPLACE("01ceecf9-d01b-4af2-b5dcf1235891044f", "-", "")), "01234", "ajike@gmail.com", "h38whs8", "greatuser");
+
+INSERT INTO fan (fanId, fanActivationToken, fanEmail, fanHash, fanUsername)
+VALUES (UNHEX(REPLACE("f5ea6c2e-9d3c-4207-83c5-aa0009d130ea", "-", "")), "52637", "qwertt@qwerty.com", "a349r0", "thatuser");
+
+UPDATE fan SET fanEmail = 'newguy@guy.com' WHERE fanUsername = 'greatuser';
+UPDATE fan SET fanEmail = 'topgun@apex.com' WHERE fanUsername = 'thatuser';
+
+DELETE FROM fan WHERE fanUsername = 'thatuser';
+DELETE FROM fan WHERE fanUsername = 'greatuser';
+
