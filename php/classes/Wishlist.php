ade5747f72800812ad3041e90df942a4ea522d2f
Brent Kie <brent.a.kie@gmail.com>
2018-04-26 16:25:09 -0600

    Changed file path for the autoload.php in all classes.

--- php/classes/Wishlist.php
+++ php/classes/Wishlist.php
@@ -5,7 +5,7 @@ namespace Edu\Cnm\DataDesign;
 
 
 require_once("autoload.php");
-require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
